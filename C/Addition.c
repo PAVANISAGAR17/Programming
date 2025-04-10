008a490feb9205127c5ed1da324ab42b31ff3ed8
Pavanisagar17 <pavanisagar2006@gmail.com>
2025-04-10 22:15:53 +0530

    Deleted backup files

--- C/Addition.c
+++ C/Addition.c
@@ -3,10 +3,10 @@
 int main()
 {
 	int a,b;
-	prinf("Enter two values: ");
+	printf("Enter two values: ");
 	scanf("%d %d",&a,&b);//storing two values in a and b
 	int c=a+b;//adding a and b, and storing in c
-	print("%d + %d = %d\n",a,b,c);
+	printf("%d + %d = %d\n",a,b,c);
 	return 0;
 }
 
