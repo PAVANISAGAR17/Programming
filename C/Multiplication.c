69b13259e6a258895c15d75db23ee6ecc8520439
PAVANISAGAR17 <pavanisagar2006@gmail.com>
2025-04-10 14:46:02 +0530

    Create Multiplication.c

---
+++ C/Multiplication.c
@@ -0,0 +1,11 @@
+//C program to print Product of Two Numbers.
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter two values: ");
+  scanf("%d %d",&a,&b);
+  int c=a*b;
+  printf("%d X %d = %d\n",a,b,c);
+  return 0;
+}
