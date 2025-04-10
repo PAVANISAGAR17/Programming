0d64690babbdf3bfb5a9e9adc5bed0bc1467d77d
pavanisagar17 <pavanisagar2006@gmail.com>
2025-04-10 13:52:57 +0530

    Added DynamicInput Program

---
+++ C/DynamicInput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a Value: ");//Prompt to ask user
+	scanf("%d",&a);//function to get input frok keyboard
+	printf("%d\n",a);//Printing a
+	return 0;
+}
+
